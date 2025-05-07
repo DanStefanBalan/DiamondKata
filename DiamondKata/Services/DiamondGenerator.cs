c62a39a45524739726305a84340f66ebfca452c9
Dan Balan <Dan.Balan@mbg.cognizant.com>
2025-05-07 10:07:01 +0300

    Fixing the latest failed unit test

--- DiamondKata/Services/DiamondGenerator.cs
+++ DiamondKata/Services/DiamondGenerator.cs
@@ -15,8 +15,39 @@ public sealed class DiamondGenerator : IDiamondGenerator
                 { 'A' }
             };
         }
+        
+        var currentCharacter = 'A';
+        var positionTracker = targetLetter - 'A'; 
+        var halfDiamondHeight = positionTracker;
+        var matrix = new char[positionTracker * 2 + 1, positionTracker * 2 + 1];
 
-        return new char[0,0];
+        for (var rowIndex = 0; rowIndex < matrix.GetLength(0); rowIndex++)
+        {
+            for (var colIndex = 0; colIndex < matrix.GetLength(1); colIndex++)
+            {
+                if (colIndex == positionTracker || colIndex == matrix.GetLength(1) - positionTracker - 1)
+                {
+                    matrix[rowIndex, colIndex] = currentCharacter;
+                }
+                else
+                {
+                    matrix[rowIndex, colIndex] = ' ';
+                }
+            }
+
+            if (rowIndex < halfDiamondHeight)
+            {
+                currentCharacter++;
+                positionTracker--;
+            }
+            else
+            {
+                currentCharacter--;
+                positionTracker++;
+            }
+        }
+
+        return matrix;
     }
     
     private static void ValidateInput(char targetLetter)
