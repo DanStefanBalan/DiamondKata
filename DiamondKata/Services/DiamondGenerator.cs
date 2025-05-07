25c39d5b422efb12a3b14d63e32a2a85c46313b3
Dan Balan <Dan.Balan@mbg.cognizant.com>
2025-05-07 10:24:25 +0300

    Refactoring - organizing the code better in different methods to improve readability and the maintainability

--- DiamondKata/Services/DiamondGenerator.cs
+++ DiamondKata/Services/DiamondGenerator.cs
@@ -8,32 +8,48 @@ public sealed class DiamondGenerator : IDiamondGenerator
     {
         ValidateInput(targetLetter);
         
-        if (targetLetter == 'A')
+        var size = GetMatrixSize(targetLetter);
+        var matrix = InitializeMatrix(size);
+        FillMatrix(matrix, targetLetter);
+
+        return matrix;
+    }
+    
+    private static void ValidateInput(char targetLetter)
+    {
+        if (targetLetter is < 'A' or > 'Z')
         {
-            return new[,]
-            {
-                { 'A' }
-            };
+            throw new ArgumentException("Input must be a letter from A-Z.");
         }
-        
+    }
+    
+    private static int GetMatrixSize(char targetLetter) => (targetLetter - 'A') * 2 + 1;
+    
+    private static char[,] InitializeMatrix(int size)
+    {
+        var matrix = new char[size, size];
+
+        for (var rowIndex = 0; rowIndex < size; rowIndex++)
+        {
+            for (var colIndex = 0; colIndex < size; colIndex++)
+            {
+                matrix[rowIndex, colIndex] = ' ';
+            }
+        }
+
+        return matrix;
+    }
+
+    private static void FillMatrix(char[,] matrix, char targetLetter)
+    {
         var currentCharacter = 'A';
         var positionTracker = targetLetter - 'A'; 
         var halfDiamondHeight = positionTracker;
-        var matrix = new char[positionTracker * 2 + 1, positionTracker * 2 + 1];
 
         for (var rowIndex = 0; rowIndex < matrix.GetLength(0); rowIndex++)
         {
-            for (var colIndex = 0; colIndex < matrix.GetLength(1); colIndex++)
-            {
-                if (colIndex == positionTracker || colIndex == matrix.GetLength(1) - positionTracker - 1)
-                {
-                    matrix[rowIndex, colIndex] = currentCharacter;
-                }
-                else
-                {
-                    matrix[rowIndex, colIndex] = ' ';
-                }
-            }
+            matrix[rowIndex, positionTracker] = currentCharacter;
+            matrix[rowIndex, matrix.GetLength(1) - positionTracker - 1] = currentCharacter;
 
             if (rowIndex < halfDiamondHeight)
             {
@@ -45,16 +61,6 @@ public sealed class DiamondGenerator : IDiamondGenerator
                 currentCharacter--;
                 positionTracker++;
             }
-        }
-
-        return matrix;
-    }
-    
-    private static void ValidateInput(char targetLetter)
-    {
-        if (targetLetter is < 'A' or > 'Z')
-        {
-            throw new ArgumentException("Input must be a letter from A-Z.");
         }
     }
 }
