4a3459e79fa48c105698d845ea81146e2c57d2a0
Dan Balan <Dan.Balan@mbg.cognizant.com>
2025-05-07 09:42:20 +0300

    Added unit test for having input letter B - which is going to fail for the moment

--- DiamondKata.Tests/DiamondGeneratorTests.cs
+++ DiamondKata.Tests/DiamondGeneratorTests.cs
@@ -43,4 +43,28 @@ public sealed class DiamondGeneratorTests
         var ex = Assert.Throws<ArgumentException>(() => generator.GenerateDiamondMatrix('1'));
         Assert.That(ex.Message, Is.EqualTo("Input must be a letter from A-Z."));
     }
+    
+    [Test]
+    public void GenerateDiamondMatrix_GivenLetterB_ReturnMatrixHaving3ColumnsAnd3RowWithLetterBInCenter()
+    {
+        // Arrange
+        IDiamondGenerator generator = new DiamondGenerator();
+        char[,] expectedMatrix =
+        {
+            { ' ', 'A', ' ' },
+            { 'B', ' ', 'B' },
+            { ' ', 'A', ' ' }
+        };
+
+        // Act
+        var result = generator.GenerateDiamondMatrix('B');
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expectedMatrix));
+            Assert.That(result.GetLength(0), Is.EqualTo(3));
+            Assert.That(result.GetLength(1), Is.EqualTo(3));
+        });
+    }
 }
